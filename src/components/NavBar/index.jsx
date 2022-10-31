7bed91439d4b52aec9d2e30531182eb1e5e3a8b1
v1ab1 <sharkandmouse@gmail.com>
2022-11-01 00:14:36 +0500

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,7 +3,28 @@ import style from './NavBar.module.sass';
 function NavBar() {
     return (
       <div className={style.NavBar}>
-        
+        <div>
+            <a href="">
+                
+            </a>
+            <a href="">
+                
+            </a>
+            <a href="">
+                
+            </a>
+            <a href="">
+                
+            </a>
+            <a href="">
+                
+            </a>
+        </div>
+        <div>
+            <button>
+                Callback
+            </button>
+        </div>
       </div>
     );
   }
