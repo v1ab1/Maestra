2623a00ab5510d70e6d3ecb6acd00c89e1441c73
v1ab1 <sharkandmouse@gmail.com>
2022-10-31 23:25:38 +0500

    work on header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
