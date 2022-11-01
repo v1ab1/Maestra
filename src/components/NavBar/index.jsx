a0d3b7aa8d8b0c1fe9655c7cee1e4ed4b099fa91
v1ab1 <sharkandmouse@gmail.com>
2022-11-01 18:51:19 +0500

    lil upd

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,24 +5,26 @@ function NavBar() {
       <div className={style.NavBar}>
         <div>
             <a href="">
-                
+              О студии
             </a>
             <a href="">
-                
+              Команда
             </a>
             <a href="">
-                
+              Продукты
             </a>
             <a href="">
-                
+              Условия
             </a>
             <a href="">
-                
+              Контакты и локации
             </a>
         </div>
         <div>
             <button>
+              <span>
                 Callback
+              </span>
             </button>
         </div>
       </div>
